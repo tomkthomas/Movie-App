2a14892f4be3aac6abc807b3735d00afbc5e0c07
Tom Thomas <tomthomas@Toms-MacBook-Pro.local>
2021-10-14 20:12:50 +0530

    movie-app

---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,58 @@
+import { useState, useEffect, useRef } from "react";
+//api
+import API from '../API';
+
+const initialState = {
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0
+};
+
+export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [state, setState] = useState(initialState);
+    const [loading,setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [loadMore, setLoadMore] = useState(false);
+
+    //console.log(searchTerm);
+
+    const fetchMovies = async(page, searchTerm = "") => {
+        try{
+            setError(false);
+            setLoading(true);
+            
+            const movies = await API.fetchMovies(searchTerm, page);
+            
+
+            setState(prev => ({
+                ...movies,
+                results: page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+            
+        }catch(error){
+            
+            setError(true);
+        }
+        setLoading(false);
+        
+    };
+
+
+    //initial render and search
+    useEffect(() => {
+        setState(initialState);
+        fetchMovies(1,searchTerm);
+    },[searchTerm]);
+
+    //load more
+    useEffect (()=> {
+        if(!loadMore) return;
+    
+        fetchMovies(state.page + 1, searchTerm);
+        setLoadMore(false);
+    },[loadMore, searchTerm, state.page])
+
+    return {state, loading, error, setSearchTerm,searchTerm, setLoadMore};
+};
